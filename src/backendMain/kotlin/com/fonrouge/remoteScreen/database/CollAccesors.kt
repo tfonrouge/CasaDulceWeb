efc5ae8f8760f3ef2461e466104647713f8e9ea6
Teo Fonrouge <teo@fonrouge.com>
2022-05-30 03:00:41 -0500

    * update

--- src/backendMain/kotlin/com/fonrouge/remoteScreen/database/CollAccesors.kt
+++ src/backendMain/kotlin/com/fonrouge/remoteScreen/database/CollAccesors.kt
@@ -72,7 +72,7 @@ suspend inline fun <reified T : Any> CoroutineCollection<T>.buildRemoteData(
 
     val pipeline = mutableListOf<Bson>()
 
-    val filterValue = (match as? Document)?: Document()
+    val filterValue = (match as? Document) ?: Document()
 
     if (filter != null && filter.isNotEmpty()) {
         filter.forEach { remoteFilter ->
