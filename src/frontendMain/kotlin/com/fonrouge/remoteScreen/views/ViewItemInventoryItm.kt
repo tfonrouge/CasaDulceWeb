a40fbf8dac1d2ee7f56df6e88656943078964766
Teo Fonrouge <teo@fonrouge.com>
2022-08-14 17:14:23 -0500

    * update

--- src/frontendMain/kotlin/com/fonrouge/remoteScreen/views/ViewItemInventoryItm.kt
+++ src/frontendMain/kotlin/com/fonrouge/remoteScreen/views/ViewItemInventoryItm.kt
@@ -4,9 +4,15 @@ import com.fonrouge.fsLib.lib.UrlParams
 import com.fonrouge.fsLib.view.ViewItem
 import com.fonrouge.remoteScreen.config.ConfigViewImpl
 import com.fonrouge.remoteScreen.model.InventoryItm
+import io.kvision.core.Container
+import io.kvision.form.FormPanel
 
 class ViewItemInventoryItm(
     override var urlParams: UrlParams?
 ) : ViewItem<InventoryItm, String>(
     configView = ConfigViewImpl.ConfigViewItemInventoryItm,
-)
+) {
+    override fun Container.pageItemBody(): FormPanel<InventoryItm>? {
+        TODO("Not yet implemented")
+    }
+}
