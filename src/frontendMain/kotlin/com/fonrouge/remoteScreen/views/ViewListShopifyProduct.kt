97f71911f327f7c6318a90f6e522d79660d33ac4
Israel <israel@fonrouge.com>
2022-11-07 15:50:50 -0600

    * Update

--- src/frontendMain/kotlin/com/fonrouge/remoteScreen/views/ViewListShopifyProduct.kt
+++ src/frontendMain/kotlin/com/fonrouge/remoteScreen/views/ViewListShopifyProduct.kt
@@ -6,16 +6,15 @@ import com.fonrouge.fsLib.lib.UrlParams
 import com.fonrouge.fsLib.view.AppScope
 import com.fonrouge.fsLib.view.ViewList
 import com.fonrouge.remoteScreen.config.ConfigViewImpl.Companion.ConfigViewListShopifyProduct
-import com.fonrouge.remoteScreen.model.ModelShopifyApiService
 import com.fonrouge.remoteScreen.model.ShopifyProduct
 import com.fonrouge.remoteScreen.services.DataListService
 import com.fonrouge.remoteScreen.services.ShopifyApiService
+import csstype.HtmlAttributes
 import io.kvision.core.Container
 import io.kvision.html.*
 import io.kvision.panel.hPanel
 import io.kvision.panel.vPanel
 import io.kvision.tabulator.ColumnDefinition
-import io.kvision.tabulator.Editor
 import io.kvision.tabulator.js.Tabulator
 import io.kvision.utils.px
 import kotlinx.coroutines.launch
@@ -32,22 +31,18 @@ class ViewListShopifyProduct(
         ColumnDefinition(
             title = "Barcode",
             field = fieldName(ShopifyProduct::barcode),
-            headerFilter = Editor.INPUT,
         ),
         ColumnDefinition(
             title = "Handle",
             field = fieldName(ShopifyProduct::handle),
-            headerFilter = Editor.INPUT,
         ),
         ColumnDefinition(
             title = "title",
-            field = fieldName(ShopifyProduct::title),
-            headerFilter = Editor.INPUT,
+            field = fieldName(ShopifyProduct::title)
         ),
         ColumnDefinition(
             title = "bodyHtml",
-            field = fieldName(ShopifyProduct::bodyHtml),
-            headerFilter = Editor.INPUT,
+            field = fieldName(ShopifyProduct::bodyHtml)
         ),
         ColumnDefinition(
             title = "createdAt",
@@ -55,8 +50,8 @@ class ViewListShopifyProduct(
         ),
         ColumnDefinition(
             title = "tags",
-            field = fieldName(ShopifyProduct::tags),
-            headerFilter = Editor.INPUT,
+            field = fieldName(ShopifyProduct::tags)
+
         ),
         ColumnDefinition(
             title = "image",
@@ -79,13 +74,13 @@ class ViewListShopifyProduct(
         ColumnDefinition(
             title = "images",
             fieldName(ShopifyProduct::images),
-            formatterComponentFunction = { _, _, data ->
-                data.images.let { src ->
+            formatterComponentFunction = { _, _, span ->
+                span.images.let { src ->
                     Image(src.toString()) {
                         width = 35.px
                         height = 35.px
                     }
-                }
+                } ?: Div("no image")
             }
         )
     )
@@ -108,12 +103,6 @@ class ViewListShopifyProduct(
                     }
                 }
             }
-        }
-    }
-
-    init {
-        AppScope.launch {
-            ModelShopifyApiService.shopifyApiService.syncFromShopify()
         }
     }
 }
